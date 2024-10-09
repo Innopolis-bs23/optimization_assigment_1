b08ae44ab4b9230abd119072421bc77dd44d9c25
Mikhail Panteleev <m.panteleev@innopolis.university>
2024-10-10 02:48:12 +0300

    Fix to output X vector

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def check_solved(c, A, b):
             return False  
     return True
 
-def simplex_step(c, A, b, solution, eps, Matrix):
+def simplex_step(c, A, b, solution, eps, X):
     # find pivot
     pivot = None
     min_for_pivot = 10**100
@@ -13,7 +13,7 @@ def simplex_step(c, A, b, solution, eps, Matrix):
             min_for_pivot = c[i]
             pivot = i
     if pivot is None:   # solved already
-        return c, A, b, solution, Matrix, True
+        return c, A, b, solution, X, True
     min_ratio = None
     row = None
     for i in range(len(A)):
@@ -24,10 +24,10 @@ def simplex_step(c, A, b, solution, eps, Matrix):
             if (min_ratio is None or ratio < min_ratio) and ratio > 0:  # find the minimum ratio
                 min_ratio = ratio
                 row = i
-    # update the matrix
+    # update the X
     if min_ratio is None: # check if solvable
-        return c, A, b,solution, Matrix, False
-    divider = A[row][pivot] #save our value 
+        return c, A, b,solution, X, False
+    divider = A[row][pivot] # save our value 
     b[row] = b[row] / divider # normalize the pivot row
     for i in range(len(A[row])):
         A[row][i] = A[row][i] / divider
@@ -44,48 +44,47 @@ def simplex_step(c, A, b, solution, eps, Matrix):
         c[i] -= c_factor * A[row][i]
         
     solution -= c_factor * b[row]
-    Matrix[pivot] = row
-    return c, A, b,solution, Matrix, True
+    X[pivot] = row
+    return c, A, b,solution, X, True
     
 
 def simplex_method(c, A, b, epsilon, maximize=True):
     solution = 0
-    Matrix = [None for i in range(len(c))]
+    X = [None for _ in range(len(c))]
     ifSolvable = True
     checkForAccuracy = 0
     if maximize:
         c = [-i for i in c]
     while not check_solved(c, A, b):
         if ifSolvable is False: # check if unbounded
-            return c, A, b, solution, Matrix, ifSolvable
-        c, A, b, solution, Matrix, ifSolvable = simplex_step(c, A, b,solution, epsilon, Matrix)
-        if abs(checkForAccuracy - solution) <= epsilon:
-            if (not maximize):
-                solution *= -1 # check for accuracy
-            for i in range(len(Matrix)):
-                if(Matrix[i] is None):
-                    Matrix[i] = 0
-                else:
-                    Matrix[i] = b[Matrix[i]]    
-            return c, A, b, solution, Matrix, ifSolvable
+            return c, A, b, solution, X, ifSolvable
+        c, A, b, solution, X, ifSolvable = simplex_step(c, A, b, solution, epsilon, X)
+        if abs(checkForAccuracy - solution) <= epsilon: # check for accuracy
+            if not maximize:
+                solution *= -1 
+            for i in range(len(X)):
+                X[i] = 0
+                if X[i] is not None:
+                    X[i] = b[X[i]]
+            return c, A, b, solution, X, ifSolvable
         checkForAccuracy = solution
-    for i in range(len(Matrix)):
-        if(Matrix[i] is None):
-            Matrix[i] = 0
+    for i in range(len(X)):
+        if X[i] is None:
+            X[i] = 0
         else:
-            Matrix[i] = b[Matrix[i]]
+            X[i] = b[X[i]]
     if(not maximize):
                 solution *= -1            
-    return c, A, b, solution, Matrix, ifSolvable
+    return c, A, b, solution, X, ifSolvable
 
 
 def main():
     # Vector of coefficients of the objective function
     c = [float(i) for i in input("Vector of coefficients of the objective function: ").split()]
-    # Matrix of coefficients of the constraints
+    # X of coefficients of the constraints
     A = []
     while True:
-        row_input = input("Row of matrix of the coefficients: ")
+        row_input = input("Row of X of the coefficients: ")
         if row_input == "":
             break
         row = list(map(float, row_input.split()))
@@ -96,11 +95,11 @@ def main():
     maximize = input("max/min: ") == "max"
     # The approximation accuracy
     epsilon  = float(input("The approximation accuracy: "))
-    c, A, b, solution, matrix, ifSolvable = simplex_method(c, A, b, epsilon, maximize)
+    c, A, b, solution, X, ifSolvable = simplex_method(c, A, b, epsilon, maximize)
     if(ifSolvable):
         print("Optimal solution:")
         print(solution)
-        print(matrix)
+        print(X)
     else:
         print("Unbounded solution")    
         
--- tests.py
+++ tests.py
@@ -5,9 +5,7 @@ def check_float_equal(a, b, epsilon):
     return abs(a - b) <= epsilon
 
 def check_lists_equal(a, b, epsilon):
-    if len(a) != len(b):
-        return False
-    for i in range(len(a)):
+    for i in range(min(len(a), len(b))):
         if not check_float_equal(a[i], b[i], epsilon):
             return False
     return True
@@ -20,7 +18,38 @@ def check_matrix_equal(a, b, epsilon):
             return False
     return True
 
+# Lab 3.1
 def test_simplex_method():
+    c = [9, 10, 16, 0, 0, 0]
+    A = [[18, 15, 12, 1, 0, 0], 
+         [6, 4, 8, 0, 1, 0], 
+         [5, 3, 3, 0, 0, 1]]
+    b = [360, 192, 180]
+    epsilon = 0.01
+    maximize = True
+    c, A, b, solution, X, _ = simplex_method(c, A, b, epsilon, maximize)
+    
+    print(*X)
+    
+    assert check_float_equal(solution, 400, epsilon)
+    assert check_matrix_equal(A, 
+                              [[1, 1, 0, 1/9, -1/6, 0],
+                              [3/4 - 1/2, 0, 1, -1/9/2, 1/8+1/6/2, 0],
+                              [11/4-3/2, 0, 0, -3/9/2, -3/8+3/2/6, 1]],
+                              epsilon) 
+    assert  check_lists_equal(c, 
+                              [5, 0, 0, 2/9, 5/3, 0],
+                              epsilon)
+    assert  check_lists_equal(b,
+                              [8, 20, 96],
+                              epsilon)
+    assert  check_lists_equal(X,
+                              [0, 8, 20],
+                              epsilon)
+                
+                
+# Lab 3.2
+def test_simplex_method2():
     c = [2, 3, 0, -1, 0, 0]
     A = [[2, -1, 0, -2, 1, 0], 
          [3, 2, 1, -3, 0, 0], 
@@ -28,7 +57,7 @@ def test_simplex_method():
     b = [16, 18, 24]
     epsilon = 0.01
     maximize = True
-    c, A, b, solution, matrix, _ = simplex_method(c, A, b, epsilon, maximize)
+    c, A, b, solution, X, _ = simplex_method(c, A, b, epsilon, maximize)
     
     assert check_float_equal(solution, 25.636363636363637, epsilon)
     assert check_matrix_equal(A, 
@@ -43,30 +72,8 @@ def test_simplex_method():
                              [254/11, 6/11, 90/11], 
                              epsilon)
         
-def test_simplex_method2():
-    c = [9, 10, 16, 0, 0, 0]
-    A = [[18, 15, 12, 1, 0, 0], 
-         [6, 4, 8, 0, 1, 0], 
-         [5, 3, 3, 0, 0, 1]]
-    b = [360, 192, 180]
-    epsilon = 0.01
-    maximize = True
-    c, A, b, solution, matrix, _ = simplex_method(c, A, b, epsilon, maximize)
     
-    assert check_float_equal(solution, 400, epsilon)
-    assert check_matrix_equal(A, 
-                              [[1, 1, 0, 1/9, -1/6, 0],
-                              [3/4 - 1/2, 0, 1, -1/9/2, 1/8+1/6/2, 0],
-                              [11/4-3/2, 0, 0, -3/9/2, -3/8+3/2/6, 1]],
-                              epsilon) 
-    assert  check_lists_equal(c, 
-                              [5, 0, 0, 2/9, 5/3, 0],
-                              epsilon)
-    assert  check_lists_equal(b,
-                              [8, 20, 96],
-                              epsilon)
-                
-    
+    # Lab 3.3
 def test_simplex_method3():
     c = [-2, 2, -6, 0, 0, 0]
     A = [[2, 1, -2, 1, 0 ,0], 
@@ -75,7 +82,7 @@ def test_simplex_method3():
     b = [24, 23, 10]
     epsilon = 0.01
     maximize = False
-    c, A, b, solution, matrix, _ = simplex_method(c, A, b, epsilon, maximize)
+    c, A, b, solution, X, _ = simplex_method(c, A, b, epsilon, maximize)
     
     assert check_float_equal(solution, 
                              -123/4,
@@ -90,4 +97,7 @@ def test_simplex_method3():
                              epsilon)
     assert check_lists_equal(b,
                              [34, 3/4, 43/8], 
+                             epsilon)
+    assert check_lists_equal(X,
+                             [0, 3/4, 43/8],
                              epsilon)
